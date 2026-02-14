25914bdf94d96ef471da226e04ed230226923328
Asiandayboy <russellcheeseday@gmail.com>
2026-02-14 03:19:08 -0500

    bruh

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -39,6 +39,7 @@ vim.o.updatetime = 250  -- delay before hover
 vim.cmd [[autocmd CursorHold * lua vim.diagnostic.open_float(nil, { focusable = false, source = true, border = "rounded" })]]
 
 vim.opt.shell = "powershell"
+vim.opt.clipboard = "unnamedplus"
 
 vim.opt.showtabline = 2
 function _G.MyTabLabel()
